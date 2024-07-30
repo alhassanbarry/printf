627655d154b2f2bebd2e50a8e920cf1ada694d5c
ALHASSANE BARRY <albarry7991@gmail.com>
2024-07-30 07:14:53 -0700

    task6

--- print_pointer.c
+++ print_pointer.c
@@ -2,15 +2,16 @@
 
 /**
  * print_pointer - prints a pointer.
- * @val: value.
- * Return: int.
+ * @val: arguments.
+ * Return: counter.
  */
 int print_pointer(va_list val)
 {
 	void *p;
 	char *s = "(nil)";
-	long int a;
-	int i, b;
+	long int x;
+	int y;
+	int i;
 
 	p = va_arg(val, void*);
 	if (p == NULL)
@@ -22,9 +23,9 @@ int print_pointer(va_list val)
 		return (i);
 	}
 
-	a = (unsigned long int)p;
+	x = (unsigned long int)p;
 	_putchar('0');
 	_putchar('x');
-	b = print_hex_extra(x);
-	return (b + 2);
+	y = print_hex_extra(x);
+	return (y + 2);
 }
