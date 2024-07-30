5d59544f5f372839c18aa7ddfcccd0b78cae654d
ALHASSANE BARRY <albarry7991@gmail.com>
2024-07-30 07:11:04 -0700

    task 6

--- main.h
+++ main.h
@@ -26,7 +26,7 @@ int print_pointer(va_list val);
 int print_unsigned(va_list args);
 int print_hex_extra(unsigned long int num);
 int _strlenc(const char *s);
-int print_HEX_extra(unsigned int num);
+int print_HEX_extra(unsigned long int num);
 int print_exc_string(va_list val);
 int print_HEX(va_list val);
 int print_hex(va_list val);
